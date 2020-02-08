ae3075af929c1eeeb3a8e67aaa695d0d5d80bbb6
LonerChenrui <1823449792@qq.com>
2020-02-08 16:06:29 +0800

    1

--- servers/network.js
+++ servers/network.js
@@ -1,5 +1,3 @@
-const baseUrl1 = "http://106.54.54.237:8000/api/hy";
-const baseUrl2 = "http://123.207.32.32:8000/api/hy";
 
 export default function request(option) {
   return new Promise((resolve,reject) => {
